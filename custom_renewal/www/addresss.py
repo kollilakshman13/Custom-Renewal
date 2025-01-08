6f5b3acadf3725e9a9ef24a8fa220038956bf249
kollilakshman13 <kollilakshman13@gmail.com>
2025-01-08 17:51:36 +0530

    chore 08-01-2025

--- custom_renewal/www/addresss.py
+++ custom_renewal/www/addresss.py
@@ -1,30 +1,64 @@
+
 import frappe
 
 def get_context(context):
     user_email = frappe.session.user
 
-    # Check if the user is not a guest
-    if user_email != "Guest":
-        # If the user is Administrator, show all issues
-        if user_email == "Administrator":
-            context.doc = frappe.get_all(
-                "Issue",
-                fields=["*"],
-                order_by="creation desc",
-                limit_page_length=20
-            )
-        else:
-            # If the user is not Administrator, filter by raised_by
-            context.doc = frappe.get_all(
-                "Issue",
-                fields=["*"],
-                filters={"raised_by": user_email},
-                order_by="creation desc",
-                limit_page_length=20
-            )
-        context.message = "Showing all issues."
-    else:     
-        context.doc = []
-        context.message = "Please log in to view issues."   
+    # Administrator logic
+    if user_email == "Administrator":
+        context.doc = frappe.get_all(
+            "Address",
+            fields=["*"],
+            order_by="creation desc",
+            limit_page_length=20
+        )
+        context.message = "Showing all Address lists as Administrator."
+        return context
 
-    return context       
+    # Guest logic
+    if user_email == "Guest":
+        context.doc = []
+        context.message = "Please log in to view address lists."
+        return context
+
+    # Logic for logged-in users
+    contact_name = frappe.db.get_value("Contact", {"email_id": user_email}, "name")
+    if contact_name:
+        # Fetch the linked Customer for the logged-in Contact
+        customer = frappe.db.get_value(
+            "Dynamic Link",
+            {"parenttype": "Contact", "parent": contact_name, "link_doctype": "Customer"},
+            "link_name"
+        )
+    else:
+        customer = None
+
+    if customer:
+        # Fetch addresses specifically linked to the Customer
+        query = """
+            SELECT 
+                addr.name,addr.address_title, addr.address_line1, addr.address_line2, addr.city, 
+                addr.state, addr.country, addr.pincode,addr.email_id,addr.phone,addr.fax,addr.tax_category
+            FROM 
+                `tabAddress` AS addr
+            INNER JOIN 
+                `tabDynamic Link` AS dl 
+            ON 
+                dl.parent = addr.name
+            WHERE 
+                dl.link_doctype = 'Customer' AND dl.link_name = %s
+            ORDER BY 
+                addr.creation DESC
+            LIMIT 20
+        """
+        context.doc = frappe.db.sql(query, (customer,), as_dict=True)
+        context.message = f"Showing address lists for customer: {customer}."
+    else:
+        context.doc = []
+        context.message = "You are not authorized to view address lists."
+
+    return context
+
+
+
+
--- custom_renewal/www/issue.py
+++ custom_renewal/www/issue.py
@@ -1,11 +1,57 @@
+# import frappe
+
+# def get_context(context):
+#     user_email = frappe.session.user
+
+#     # Check if the user is not a guest
+#     if user_email != "Guest":
+#         # If the user is Administrator, show all issues
+#         if user_email == "Administrator":
+#             context.doc = frappe.get_all(
+#                 "Issue",
+#                 fields=["*"],
+#                 order_by="creation desc",
+#                 limit_page_length=20
+#             )
+#         else:
+#             contact_name = frappe.db.get_value("Contact", {"email_id": user_email}, "name")
+#             if contact_name:
+#                 customer = frappe.db.get_value(
+#                     "Dynamic Link",
+#                     {"parenttype": "Contact", "parent": contact_name, "link_doctype": "Customer"},
+#                     "link_name"
+#                 )
+#             else:
+#                 customer = None
+#             # If the user is not Administrator, filter by raised_by
+#             if customer:
+#                 context.doc = frappe.get_all(
+#                     "Issue",
+#                     fields=["*"],
+#                     filters={"customer": customer},
+#                     order_by="creation desc",
+#                     limit_page_length=20
+#                 )
+#             context.message = "Showing all issues."
+#     else:     
+#         context.doc = []
+#         context.message = "Please log in to view issues."   
+
+#     return context       
+
+
 import frappe
 
 def get_context(context):
     user_email = frappe.session.user
 
-    # Check if the user is not a guest
+    # Initialize default values
+    context.doc = []
+    context.message = "Please log in to view issues."
+
+    # If the user is not a guest
     if user_email != "Guest":
-        # If the user is Administrator, show all issues
+        # Administrator logic: Show all issues
         if user_email == "Administrator":
             context.doc = frappe.get_all(
                 "Issue",
@@ -13,18 +59,37 @@ def get_context(context):
                 order_by="creation desc",
                 limit_page_length=20
             )
+            context.message = "Showing all issues for Administrator."
+            return context
+
+        # Non-administrator users
+        contact_name = frappe.db.get_value("Contact", {"email_id": user_email}, "name")
+        if contact_name:
+            # Fetch linked Customer from the Dynamic Link table
+            customer = frappe.db.get_value(
+                "Dynamic Link",
+                {"parenttype": "Contact", "parent": contact_name, "link_doctype": "Customer"},
+                "link_name"
+            )
         else:
-            # If the user is not Administrator, filter by raised_by
+            customer = None
+
+        # If a customer is linked, show their issues
+        if customer:
             context.doc = frappe.get_all(
                 "Issue",
                 fields=["*"],
-                filters={"raised_by": user_email},
+                filters={"customer": customer},
                 order_by="creation desc",
                 limit_page_length=20
             )
-        context.message = "Showing all issues."
-    else:     
-        context.doc = []
-        context.message = "Please log in to view issues."   
+            context.message = f"Showing issues for customer: {customer}."
+        else:
+            # If no customer is linked
+            context.message = "You are not authorized to view issues."
+    else:
+        # Guest user logic
+        context.message = "Please log in to view issues."
 
-    return context       
+    return context
+
